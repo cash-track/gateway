ab6a0d7b097df575662534516a65897598c65f59
Volodymyr Komarov <vovikems@gmail.com>
2023-12-14 22:49:47 +0200

    Add logger tests

---
+++ router/router_test.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	r := New()
+
+	l := r.List()
+
+	assert.Len(t, l, 2)
+
+	assert.NotNil(t, l["*"])
+	assert.Len(t, l["*"], 3)
+	assert.Contains(t, l["*"], "/live")
+	assert.Contains(t, l["*"], "/ready")
+	assert.Contains(t, l["*"], "/api/{path:*}")
+
+	assert.NotNil(t, l["POST"])
+	assert.Len(t, l["POST"], 4)
+	assert.Contains(t, l["POST"], "/api/auth/login")
+	assert.Contains(t, l["POST"], "/api/auth/logout")
+	assert.Contains(t, l["POST"], "/api/auth/register")
+	assert.Contains(t, l["POST"], "/api/auth/provider/google")
+}
